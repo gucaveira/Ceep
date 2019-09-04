a8c514b3110e46b2a610d41da6ef7ab1da164041
gustavo pereira <pereiragustavo215@gmail.com>
2019-09-04 11:40:57 -0300

    Initial commit

---
+++ app/src/main/java/com/ceep/dao/NotaDao.java
@@ -0,0 +1,37 @@
+package com.ceep.dao;
+
+import com.ceep.model.Nota;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class NotaDao {
+
+    private static final ArrayList<Nota> notas = new ArrayList();
+
+    public List<Nota> notasTodas() {
+        return (List<Nota>) notas.clone();
+    }
+
+    public void insere(Nota... notas) {
+        NotaDao.notas.addAll(Arrays.asList(notas));
+    }
+
+    public void altera(int posicao, Nota nota) {
+        notas.set(posicao, nota);
+    }
+
+    public void remove(int posicao) {
+        notas.remove(posicao);
+    }
+
+    public void troca(int posicaoInicia, int posicaoFinal) {
+        Collections.swap(notas, posicaoInicia, posicaoFinal);
+    }
+
+    public void removeTodos(){
+        notas.clear();
+    }
+}
