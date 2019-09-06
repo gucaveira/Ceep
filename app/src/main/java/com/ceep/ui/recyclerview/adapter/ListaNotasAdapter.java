88102e40e2b9db3e1b01a45391c668f923b0f8f4
gustavo pereira <pereiragustavo215@gmail.com>
2019-09-06 13:09:04 -0300

    refatorando os metodos da classe ListaNotasAdapter

--- app/src/main/java/com/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
+++ app/src/main/java/com/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -14,7 +14,7 @@ import com.ceep.model.Nota;
 
 import java.util.List;
 
-public class ListaNotasAdapter extends RecyclerView.Adapter {
+public class ListaNotasAdapter extends RecyclerView.Adapter<ListaNotasAdapter.NotaViewHolder> {
 
     private Context context;
 
@@ -27,19 +27,15 @@ public class ListaNotasAdapter extends RecyclerView.Adapter {
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ListaNotasAdapter.NotaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false);
         return new NotaViewHolder(viewCriada);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(ListaNotasAdapter.NotaViewHolder holder, int position) {
         Nota nota = notas.get(position);
-        TextView titulo = holder.itemView.findViewById(R.id.item_nota_titulo);
-        titulo.setText(nota.getTitulo());
-
-        TextView descricao = holder.itemView.findViewById(R.id.item_nota_descricao);
-        descricao.setText(nota.getDescricao());
+        holder.vincula(nota);
     }
 
     @Override
@@ -50,8 +46,19 @@ public class ListaNotasAdapter extends RecyclerView.Adapter {
 
     class NotaViewHolder extends RecyclerView.ViewHolder {
 
+        private final TextView titulo;
+        private final TextView descricao;
+
         public NotaViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            titulo = itemView.findViewById(R.id.item_nota_titulo);
+            descricao = itemView.findViewById(R.id.item_nota_descricao);
+        }
+
+        public void vincula(Nota nota) {
+            titulo.setText(nota.getTitulo());
+            descricao.setText(nota.getDescricao());
         }
     }
 
