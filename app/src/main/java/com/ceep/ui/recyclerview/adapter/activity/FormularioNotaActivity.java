3cc4aee6501a2dbedadfe7c22d6157d39250898b
gustavo pereira <pereiragustavo215@gmail.com>
2019-09-06 23:40:08 -0300

    Foram add a pasta menu e o ic salvar

--- app/src/main/java/com/ceep/ui/recyclerview/adapter/activity/FormularioNotaActivity.java
+++ app/src/main/java/com/ceep/ui/recyclerview/adapter/activity/FormularioNotaActivity.java
@@ -1,14 +1,11 @@
 package com.ceep.ui.recyclerview.adapter.activity;
 
+import android.os.Bundle;
+import android.view.Menu;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
 import com.ceep.R;
-
-import org.w3c.dom.Text;
 
 public class FormularioNotaActivity extends AppCompatActivity {
 
@@ -16,5 +13,11 @@ public class FormularioNotaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_nota);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario_nota_salva, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
